3b638d8695fd22bfee9e236e7325f35b0d8a7147
Main <main@Beast-Air-2.local>
2023-05-18 04:24:46 -0500

    fixing aws provider

--- providers.tf
+++ providers.tf
@@ -1,8 +1,7 @@
-provider "tfe" {
-  token = var.TFE_TOKEN
-}
+# provider "tfe" {
+#   token = var.TFE_TOKEN
+# }
 provider "aws" {
-  version = "~> 4.0"
   region     = "us-west-2"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
