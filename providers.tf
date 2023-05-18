1586943ea9500aaf9e6ef8595418fd7a424a5a39
Main <main@Beast-Air-2.local>
2023-05-18 03:01:46 -0500

    fixing variable definitions

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 provider "tfe" {
-  token = var.tfe_token
+  token = var.TFE_TOKEN
 }
 provider "aws" {
   region     = var.REGION
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 
-  default_tags {
+default_tags {
     tags = {
       "Automation"  = "Terraform"
       "Project"     = "WebApp for barcode data retrieval"
--- variables.tf
+++ variables.tf
@@ -6,10 +6,10 @@ variable "profile" {
   description = "Environment"
   type        = string
 }
-variable "REGION" {
-  description = "AWS Region"
-  type        = string
-}
+# variable "REGION" {
+#   description = "AWS Region"
+#   type        = string
+# }
 variable "project_name" {
   description = "project name"
   type        = string
@@ -18,18 +18,18 @@ variable "vpc_cidr" {
   description = "VPC CIDR"
   type        = string
 }
-variable "AWS_ACCESS_KEY" {
-  description = "AWS Access Key"
-  type        = string
-}
-variable "AWS_SECRET_KEY" {
-  description = "AWS Secret Key"
-  type        = string
-}
-variable "TFE_TOKEN" {
-  description = "Terraform Cloud Token"
-  type        = string
-}
+# variable "AWS_ACCESS_KEY" {
+#   description = "AWS Access Key"
+#   type        = string
+# }
+# variable "AWS_SECRET_KEY" {
+#   description = "AWS Secret Key"
+#   type        = string
+# }
+# variable "TFE_TOKEN" {
+#   description = "Terraform Cloud Token"
+#   type        = string
+# }
 
 
 
